2578984ca494cb65bc14b2c4ecd69139967763e2
div1121 <jasonchu19991121@gmail.com>
2020-11-18 11:26:44 +0800

    Source file

---
+++ source_file/Driver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+import java.sql.*;
+/**
+ *
+ * @author user
+ */
+public class Driver {
+    public Connection conn;
+    public Driver(Connection conn){
+        this.conn = conn;
+    }
+}
