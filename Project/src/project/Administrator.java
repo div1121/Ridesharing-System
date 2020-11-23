320a9aa99872859345aa24392879288a89ca46e1
syyys <48011641+syyys@users.noreply.github.com>
2020-11-23 21:49:58 +0800

    Update Administrator.java

--- Project/src/project/Administrator.java
+++ Project/src/project/Administrator.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package project;
 
 import java.sql.*;
+import java.util.*;
 /**
  *
  * @author user
@@ -43,4 +43,65 @@ public class Administrator {
     {
         System.out.println("Login as Admin");
     }
+
+    private void CreateTables()
+    {
+        System.out.println("1. Create tables");
+    }
+    private void DeleteTables()
+    {
+        System.out.println("2. Delete tables");
+    }
+    private void LoadData()
+    {
+        System.out.println("3. Load data");
+    }
+    private void CheckData()
+    {
+        System.out.println("4. Check data");
+    }
+    private void GoBack()
+    {
+        System.out.println("5. Go back");
+        Project a = new Project();
+        System.out.println("");
+        a.Menu();
+    }
+
+    void menu()
+    {
+        System.out.println("Administrator, what would you like to do?");
+        System.out.println("1. Create tables");
+        System.out.println("2. Delete tables");
+        System.out.println("3. Load data");
+        System.out.println("4. Check data");
+        System.out.println("5. Go back");
+        System.out.println("Please enter [1-5]");
+        Scanner sc = new Scanner(System.in);
+        int op = sc.nextInt();
+        switch (op)
+        {
+            case 1:
+                CreateTables();
+                break;
+            case 2:
+                DeleteTables();
+                break;
+            case 3:
+                LoadData();
+                break;
+            case 4:
+                CheckData();
+                break;
+            case 5:
+                GoBack();
+                break;
+            default:
+                System.out.println("Invalid input, please try again.");
+                menu();
+                break;
+        }
+    }
+
+
 }
