e5069d2d54958700000550ac57d5c4b2cf2afa6a
syyys <48011641+syyys@users.noreply.github.com>
2020-11-23 21:51:05 +0800

    Update Manager.java

--- Project/src/project/Manager.java
+++ Project/src/project/Manager.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package project;
 
 import java.sql.*;
+import java.util.*;
 /**
  *
  * @author user
@@ -18,5 +20,39 @@ public class Manager {
     void msg()
     {
         System.out.println("Login as Manager");
+    }
+
+    private void FindTrip()
+    {
+        System.out.println("1. Find trips");
+    }
+    private void GoBack()
+    {
+        Project a = new Project();
+        System.out.println("");
+        a.Menu();
+    }
+
+    void menu()
+    {
+        System.out.println("Manager, what would you like to do?");
+        System.out.println("1. Find trips");
+        System.out.println("2. Go back");
+        System.out.println("Please enter [1-2]");
+        Scanner sc = new Scanner(System.in);
+        int op = sc.nextInt();
+        switch (op)
+        {
+            case 1:
+                FindTrip();
+                break;
+            case 2:
+                GoBack();
+                break;
+            default:
+                System.out.println("Invalid input, please try again.");
+                menu();
+                break;
+        }
     }
 }
