8a9584b5d517ad52a7bc5dfca6ad722188baf53e
div1121 <jasonchu19991121@gmail.com>
2020-11-27 11:57:36 +0800

    Combine with Netbean

--- Project/src/project/Manager.java
+++ Project/src/project/Manager.java
@@ -6,6 +6,7 @@
 package project;
 
 import java.sql.*;
+import java.util.*;
 /**
  *
  * @author user
@@ -15,5 +16,42 @@ public class Manager {
     public Manager(Connection conn){
         this.conn = conn;
     }
-    
+    void msg()
+    {
+        System.out.println("Login as Manager");
+    }
+
+    private void FindTrip()
+    {
+        System.out.println("1. Find trips");
+    }
+    private void GoBack()
+    {
+        Project a = new Project();
+        System.out.println("");
+        a.Menu();
+    }
+
+    void menu()
+    {
+        System.out.println("Manager, what would you like to do?");
+        System.out.println("1. Find trips");
+        System.out.println("2. Go back");
+        System.out.println("Please enter [1-2]");
+        Scanner sc = new Scanner(System.in);
+        int op = sc.nextInt();
+        switch (op)
+        {
+            case 1:
+                FindTrip();
+                break;
+            case 2:
+                GoBack();
+                break;
+            default:
+                System.out.println("Invalid input, please try again.");
+                menu();
+                break;
+        }
+    }
 }
--- Project/src/project/Passenger.java
+++ Project/src/project/Passenger.java
@@ -6,6 +6,7 @@
 package project;
 
 import java.sql.*;
+import java.util.*;
 /**
  *
  * @author user
@@ -14,5 +15,51 @@ public class Passenger {
     public Connection conn;
     public Passenger(Connection conn){
         this.conn = conn;
+    }
+    void msg()
+    {
+        System.out.println("Login as Passenger");
+    }
+
+    private void ReqRide()
+    {
+        System.out.println("1. Request a ride");
+    }
+    private void  CheckTripRecord()
+    {
+        System.out.println("2. Check trip records");
+    }
+    private void GoBack()
+    {
+        Project a = new Project();
+        System.out.println("");
+        a.Menu();
+    }
+
+    void menu()
+    {
+        System.out.println("Passenger, what would you like to do?");
+        System.out.println("1. Request a ride");
+        System.out.println("2. Check trip records");
+        System.out.println("3. Go back");
+        System.out.println("Please enter [1-3]");
+        Scanner sc = new Scanner(System.in);
+        int op = sc.nextInt();
+        switch (op)
+        {
+            case 1:
+                ReqRide();
+                break;
+            case 2:
+                CheckTripRecord();
+                break;
+            case 3:
+                GoBack();
+                break;
+            default:
+                System.out.println("Invalid input, please try again.");
+                menu();
+                break;
+        }
     }
 }
